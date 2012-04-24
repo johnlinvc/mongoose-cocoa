ec8a254ea7783cb22174a3f8daeed7f9deca1835
Johnlinvc <johnlinvc@gmail.com>
2012-04-24 17:32:11 +0800

    add empty demo proj

---
+++ MongooseDemo/MongooseDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MongooseDemo
+//
+//  Created by  on 12/04/24.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
