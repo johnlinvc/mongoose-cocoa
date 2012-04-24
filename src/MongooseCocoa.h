6250906a987b4dd343d71e90e1aa4a422c6a758a
Johnlinvc <johnlinvc@gmail.com>
2012-04-24 17:40:55 +0800

    add MongooseCocoa class

---
+++ src/MongooseCocoa.h
@@ -0,0 +1,13 @@
+//
+//  MongooseCocoa.h
+//  MongooseDemo
+//
+//  Created by  on 12/04/24.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MongooseCocoa : NSObject
+
+@end
---
+++ src/MongooseCocoa.m
@@ -0,0 +1,13 @@
+//
+//  MongooseCocoa.m
+//  MongooseDemo
+//
+//  Created by  on 12/04/24.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "MongooseCocoa.h"
+
+@implementation MongooseCocoa
+
+@end
